87c225c078e0e156b7fda63aa22e6f116e8771d6
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-11-02 21:35:38 +0545

    first commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <div> I am the Home Component changed ---- ...</div>
+            <button onClick={() => console.log("Hi there!!")}>BUTTON</button>
+        </div>
+    )
+}
+
+
+export default Home;
